64866c54fbfa184343ce358ce77d6cecbada0404
elmaestrotic <45459513+elmaestrotic@users.noreply.github.com>
2024-05-22 14:33:30 -0500

    BD de conocimiento para Parcial de Lenguajes
    Debes usar prolog

---
+++ arbolito.pl
@@ -0,0 +1,56 @@
+parent(abraham,isaac). parent(isaac,jacob).
+parent(jacob,judamar). parent(judamar,fares).
+parent(judamar,zara). parent(fares,hazron).
+parent(hazron,ram). parent(ram,aminadab).
+parent(aminadab,naason). parent(naason,salma).
+parent(salma,booz). parent(booz,obed).
+parent(obed,isai). parent(isai,david).
+parent(salomon,roboam). parent(roboam,abias).
+parent(abias,asa). parent(asa,josofat).
+parent(josofat,joram). parent(joram,uzias).
+parent(uzias,jotam). parent(jotam,acaz).
+parent(acaz, ezequias). parent(ezequias,manases).
+parent(manases,amon). parent(amon,josias).
+parent(josias, joaquin). parent(joaquin,salatiel).
+parent(salatiel,zorobabel). parent(zorobabel,abiud).
+parent(abiud,ellaquim). parent(ellaquim,azor).
+parent(azor, sadoc). parent(sadoc,aquim).
+parent(aquim, eliud). parent(eliud,eleazar).
+parent(eleazar, matan). parent(matan,jacob).
+parent(jacob, jose). parent(jacob, benjamin).
+parent(jacob, ruben). parent(jacob, simeon).
+parent(jacob, levi). parent(jacob, juda).
+parent(jacob, dan). parent(jacob, neftali).
+parent(jacob, gad). parent(jacob, aser).
+parent(jose,isacar). parent(jacob, zabulon).
+parent(jacob, jose).
+parent(maria, jesus).
+male(abraham).
+male(isaac). male(jacob).
+male(fares). male(zara).
+male(hazron). male(ram).
+male(aminadab). male(naason).
+male(salma). male(booz).
+male(obed). male(isai).
+male(david). male(salomon).
+male(roboam). male(abias).
+male(asa). male(josofat).
+male(joram). male(uzias).
+male(jotam). male(acaz).
+male(ezequias). male(manases).
+male(amon). male(josias).
+male(joaquin). male(salatiel).
+male(zorobabel). male(abiud).
+male(ellaquim). male(azor).
+male(sadoc). male(aquim).
+male(eliud). male(eleazar).
+male(matan). male(jacob).
+male(jose). male(jesus).
+male(benjamin). male(ruben).
+male(simeon). male(levi).
+male(juda). male(dan).
+male(neftali). male(gad).
+male(aser). male(isacar).
+male(zabulon). male(juda).
+female(maria).female(tamar).
+female(rajab).female(rut).
