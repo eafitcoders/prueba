d11fc1386d99b5e94c15a5fe5cf74e60a40e5c6b
elmaestrotic <45459513+elmaestrotic@users.noreply.github.com>
2024-05-23 10:54:49 -0500

    Base de conocimientos para Parcial de Lenguajes
    Descargar y cargar en Prolog

---
+++ chavo.pl
@@ -0,0 +1,66 @@
+parent(tomas, pedro).
+parent(tomas, roberto).
+parent(tomas, ernesto).
+parent(tomas, alicia).
+parent(maria, pedro).
+parent(juan, tomas).
+parent(carlos, juan).
+parent(fernando, jose).
+parent(jose, pedro).
+parent(fernando, carlos).
+parent(pedro, andres).
+parent(pedro, juanjo).
+parent(pedro, chavo).
+parent(andres, quico).
+parent(andres, javier).
+parent(andres, ana).
+parent(juanjo, diego).
+parent(don_ramon, chilindrina).
+parent(don_ramon, roberto).
+parent(don_ramon, quico). % Añadido
+parent(marina, chilindrina).
+parent(marina, roberto).
+
+parent(senor_barriga, ñoño).
+parent(madre_ñoño, ñoño).
+
+parent(profesor_jirafales, pedro).
+parent(profesor_jirafales, ernesto).
+parent(madre_pedro, pedro).
+parent(madre_ernesto, ernesto).
+
+parent(florinda, quico).
+parent(padre_quico, quico).
+
+% Relaciones de género
+male(tomas).
+male(pedro).
+male(roberto).
+male(ernesto).
+male(juan).
+male(carlos).
+male(jose).
+male(andres).
+male(juanjo).
+male(chavo).
+male(quico).
+male(javier).
+male(diego).
+male(senor_barriga).
+male(profesor_jirafales).
+male(ñoño).
+male(padre_quico).
+
+female(maria).
+female(alicia).
+female(ana).
+female(chilindrina).
+female(marina).
+female(florinda).
+female(madre_ñoño).
+female(madre_pedro).
+female(madre_ernesto).
+
+% Definiciones de relaciones
+father(X,Y):-parent(X,Y), male(X).
+mother(X,Y):-parent(X,Y), female(X).
